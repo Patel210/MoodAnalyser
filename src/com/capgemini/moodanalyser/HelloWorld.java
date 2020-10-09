9939b120311479896e1916a0cfc9cea755ee61c8
Patel210 <pranavpatel2101997@gmail.com>
2020-10-09 15:47:40 +0530

    Added: Hello World program on main branch

---
+++ src/com/capgemini/moodanalyser/HelloWorld.java
@@ -0,0 +1,11 @@
+package com.capgemini.moodanalyser;
+
+/**
+ * Hello World
+ *
+ */
+public class HelloWorld {
+	public static void main(String[] args) {
+		System.out.println("Hello World!");
+	}
+}
